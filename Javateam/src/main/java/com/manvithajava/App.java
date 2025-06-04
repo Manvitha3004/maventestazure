37b5608818de5a412e53af8d402b9feb5ce8c77d
Manvitha3004 <manvithareddy3004@gmail.com>
2025-06-04 20:27:43 +0530

    azure pipeline example

---
+++ Javateam/src/main/java/com/manvithajava/App.java
@@ -0,0 +1,13 @@
+package com.manvithajava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
